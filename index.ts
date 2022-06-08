07629945898797ca2b94e337643f14b54d679a32
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2022-06-08 10:56:17 -0300

    Estudando interfaces

---
+++ index.ts
@@ -0,0 +1,29 @@
+
+//interfaces
+
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'aquático';
+    executarRugido(alturaEmDecibeis: number): void;
+}
+
+interface IFelino extends IAnimal {
+    visaoNoturna: boolean;
+}
+
+const animal: IAnimal = {
+    nome:'Elefante',
+    tipo: 'terrestre',
+    executarRugido: (alturaEmDecibeis) => (`$(alturaEmDecibeis)dB`)
+}
+
+animal.executarRugido(200)
+const felino: IFelino = {
+    nome:'Leão',
+    tipo: 'terrestre',
+    visaoNoturna: true,
+}
+
+//types
+ 
+const animal:
